55155c1f38d78b970df7b6fa013607bf51f96823
Luis Rovirosa <luisrovirosa@gmail.com>
2015-01-29 07:02:29 +0100

    Some help on index

--- index.js
+++ index.js
@@ -33,7 +33,14 @@ var emails = {
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (request, response) {
-    response.send("Hello world");
+    var EOF = "<br />";
+    var explanation = "The methods are:" + EOF +
+        "GET <a href='/email/new'>/email/new</a>&nbsp;&nbsp;&nbsp;&nbsp;-> To check if there is new email" + EOF +
+        "GET <a href='/email/1234'>/email/:id</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-> To retrieve the email information" + EOF +
+        "POST /email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-> To send an email" + EOF + EOF + EOF +
+        "The existing email id are: 1234, 2345, 3456 and 4567. If you retrieve a different id it will be generated randomly";
+
+    response.send(explanation);
 });
 
 app.get('/email/new', function (request, response) {
